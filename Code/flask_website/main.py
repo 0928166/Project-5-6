138251dba73ecf9a9dcf40d541c135ed77cc0bb6
Niels Baljon <0928166@hr.nl>
2023-01-23 14:14:42 +0100

    updated adminpage authentication
    admin page no longer accesible by non admin users using /admin-page in the url

--- Code/flask_website/main.py
+++ Code/flask_website/main.py
@@ -1,6 +1,5 @@
 from website import create_app
-
+from website import create_app
 app = create_app()
 if __name__ == '__main__':
-    #host='192.168.2.7 or something, port=custom
     app.run(debug=True)
--- Code/flask_website/website/auth.py
+++ Code/flask_website/website/auth.py
@@ -31,49 +31,49 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-@auth.route('/admin-page', methods=['GET', 'POST'])
-@login_required
-def admin_page():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        first_name = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+# @auth.route('/admin-page', methods=['GET', 'POST'])
+# @login_required
+# def admin_page():
+#     if request.method == 'POST':
+#         username = request.form.get('username')
+#         first_name = request.form.get('firstName')
+#         password1 = request.form.get('password1')
+#         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(username=username).first()
+#         user = User.query.filter_by(username=username).first()
 
-        if user:
-            flash('Username already in use', category='error')
-        elif len(username) < 4: 
-            flash('username invalid', category='error')
-        elif len(first_name) < 2:
-            flash('first name is invalid', category='error')
-        elif password1 != password2:
-            flash('Passwords don\'t match', category='error')
-        elif len(password1) < 7:
-            flash('Password must be 7 characters', category='error')
-        else: 
-            new_user = User(username=username, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
+#         if user:
+#             flash('Username already in use', category='error')
+#         elif len(username) < 4: 
+#             flash('username invalid', category='error')
+#         elif len(first_name) < 2:
+#             flash('first name is invalid', category='error')
+#         elif password1 != password2:
+#             flash('Passwords don\'t match', category='error')
+#         elif len(password1) < 7:
+#             flash('Password must be 7 characters', category='error')
+#         else: 
+#             new_user = User(username=username, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+#             db.session.add(new_user)
+#             db.session.commit()
             
-            flash('Account created!', category='succes')
-            return redirect(url_for('auth.admin_page'))
-    users = User.query.all()
-    return render_template("admin_page.html", user=current_user, users=users)   
+#             flash('Account created!', category='succes')
+#             return redirect(url_for('auth.admin_page'))
+#     users = User.query.all()
+#     return render_template("admin_page.html", user=current_user, users=users)   
 
 
-@auth.route('/delete-user', methods=['POST'])
-@login_required
-def delete_user():
-    data = json.loads(request.data)
-    userId = data['userId']
-    user = User.query.get(userId)
-    if user:
-        if user.id == userId:
-            db.session.delete(user)
-            db.session.commit()
-            return jsonify({})
+# @auth.route('/delete-user', methods=['POST'])
+# @login_required
+# def delete_user():
+#     data = json.loads(request.data)
+#     userId = data['userId']
+#     user = User.query.get(userId)
+#     if user:
+#         if user.id == userId:
+#             db.session.delete(user)
+#             db.session.commit()
+#             return jsonify({})
 
 @auth.route('/acc-pass', methods=['GET', 'POST'])
 @login_required
